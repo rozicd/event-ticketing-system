bee971ce7c71e93e8507b657d2230a3d82f68a17
Dusan Rozic <dusan_rozic@hotmail.co.uk>
2024-06-24 01:22:06 +0200

    feat: search and events microservices are now communicating with rabbitmq

--- search-microservice/src/main.rs
+++ search-microservice/src/main.rs
@@ -2,9 +2,12 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use sqlx::{Postgres, Pool, postgres::PgPoolOptions};
 use dotenv::dotenv;
 use std::env;
-use routes::config::config;
-use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties, Channel};
-
+use futures_lite::stream::StreamExt;
+use lapin::{
+    options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection,
+    ConnectionProperties, Result,
+};
+// use routes::config::config;
 
 
 #[get("/")]
@@ -21,13 +24,12 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-mod models;
-mod routes;
+// mod models;
+// mod routes;
 
 
 pub struct AppState {
     db: Pool<Postgres>,
-    amqp_channel: Channel,
 }
 
 #[actix_web::main]
@@ -49,25 +51,40 @@ async fn main() -> std::io::Result<()> {
             std::process::exit(1);
         }
     };
+
     let amqp_url = env::var("AMQP_URL").expect("AMQP_URL must be set");
     let amqp_connection = Connection::connect(&amqp_url, ConnectionProperties::default()).await.expect("Failed to connect to RabbitMQ");
     let amqp_channel = amqp_connection.create_channel().await.expect("Failed to create RabbitMQ channel");
-
-    let _ = amqp_channel.queue_declare(
-        "event_queue",
-        QueueDeclareOptions::default(),
-        FieldTable::default()
-    ).await.expect("Failed to declare RabbitMQ queue");
-
+    let mut consumer = amqp_channel
+            .basic_consume(
+                "event_queue",
+                "my_consumer",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await.expect("Failed to consume message");
+    tokio::spawn(async move {
+        while let Some(delivery) = consumer.next().await {
+        
+            let delivery = delivery.expect("error in consumer");
+            let event_json = String::from_utf8_lossy(&delivery.data);
+            println!("Received message: {}", event_json);
+            delivery
+                .ack(BasicAckOptions::default())
+                .await
+                .expect("ack");
+        }
+    });
+    
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState { db: pool.clone(), amqp_channel: amqp_channel.clone() }))
-            .configure(config)
+            .app_data(web::Data::new(AppState { db: pool.clone() }))
+            // .configure(config)
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8082))?
     .run()
     .await
 }
